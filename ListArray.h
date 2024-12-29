2e5fee4c766c6a40fa3e77b37549e517ec2b646a
PRODBAL <prodbal@alumno.upv.es@ldsic-vdi19.upvnet.upv.es>
2024-12-29 21:51:53 +0100

    array completada

--- ListArray.h
+++ ListArray.h
@@ -16,52 +16,107 @@ class ListArray : public List<T> {
 		
 		ListArray(){
 		
-			T* arr[MINSIZE];
-	        	max= 2;
+			arr = new T[MINSIZE];
+	        	max= MINSIZE;
 			int n=0;	
 		}
 	 	~ListArray(){
-			delete arr;
+			delete[] arr;
 		}
 		T operator[](int pos){
 			if(pos-1<=max && pos>=0){
-				std::cout<<arr[pos]<<std::endl;
+			    return arr[pos];
 			}
 			else{
 				throw std::out_of_range("posicion inexistente");
 			}
 		}
 		friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list){
-			for(int i=0;i<list.max;i++){
-				out<<list->arr[i]<<" ";
+			int tam = list.n;
+			out<<"List => [";
+			for(int i=0;i<tam;i++){
+				out<<std::endl<<list.arr[i];
 			}
+			out<<"]";
 			return out;
 		}
-		void resize(int new_size){
-			T aux =new T[new_size];
+	       	void resize(int new_size){
+			T* aux = new T[new_size];
 			for(int i=0;i<max;i++){
 				aux[i]=arr[i];
 		
-			}	
+			}
+		       	delete[] arr;
 			arr=aux;
 			max=new_size;
-                	delete arr;
+                        	
 		}
 		void insert(int pos, T e) override{
+		    if(n>=max){
+		        resize(max+10);
+		        
+
+		    }
+		    if(pos > size() || pos<0 ){
+		                
+		        throw std::out_of_range("posicion invalida");    		
+		       
+		    }  
+		    else {
+		    
+		        for(int i = n-1;i>pos-1;i--){
+			    arr[i+1]=arr[i];
+			}
+		        arr[pos]=e;
+		    }	
+		    
+		    n++;
 		}
-	        void append(T e) override;
-		void prepend(T e) override;
-		T remove(int pos) override;
-		T get(int pos) override{
-			return arr[pos];
+	        void append(T e) override{
+	            insert((n),e);
+				
 		
+		
+		
+		
+		}
+		void prepend(T e) override{
+		    insert(0,e);    
+		}
+		T remove(int pos) override{
+		  if(pos >= size() || pos<0 ){
+		                
+		       throw std::out_of_range("posicion invalida");    		
+		       
+		   }  
+		   else{
+		       T x = arr[pos];
+	               n--;
+		       
+	               for(int i=pos;i<size();i++){
+		           arr[i]=arr[i+1];
+			   
+		       }	       
+		       return x;
+		
+		}}
+		T get(int pos) override{
+		    if(pos >=  size() || pos<0 ){
+		                
+		        throw std::out_of_range("posicion invalida");    		
+		       
+		    }  
+	            else{
+		        return arr[pos];
+		    }
 		
 		
 		}
 		int search(T e) override{
-			int pos -1;
+			int pos =  -1;
 			for(int i=0;i<max;i++){
 				if(arr[i]==e){
+					
 					pos = i;
 					break;
 				}
@@ -70,15 +125,14 @@ class ListArray : public List<T> {
 		
 		}
 		bool empty() override{
-			bool a;
+			bool a=1;
 			if( n>0){
 				a=0;	
 			} 
-			else{
-				a=1;
-			}
+			
 			return a;
 		}
+	
 		int size() override{
 			return n;
 		}
